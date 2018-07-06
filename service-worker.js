6d349300f3aa0b9bb2ed9bc891d8d5e656b8b029
SFarida <faridagueleu@gmail.com>
2018-07-06 09:55:08 +0100

    Remove uncached scripts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-
+const cacheName = 'cache1';
 const filesToCache = [
     '/',
     '/css/main.css',
@@ -11,30 +11,30 @@ const filesToCache = [
     '/index.html'
 ];
  
-self.addEventListener( "install" , function (event) { 
-    console.log('[ServiceWorker] Install');
-    event.waitUntil(
-        caches.open("cache1").then(function(cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request.url).then(function(response) {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request.url) 
-      }).catch(function(error) {
-  
-        // Respond with custom offline page
-  
-      })
-    );
-  });
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request.url).then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request.url) 
+    }).catch(function(error) {
+
+      // Respond with custom offline page
+
+    })
+  );
+});
