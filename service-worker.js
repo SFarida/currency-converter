e94a7b5cb4968578a21d1697f9d3929d9f1cb851
SFarida <faridagueleu@gmail.com>
2018-07-06 07:46:02 +0100

    Debug the service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,22 @@
 
+const filesToCache = [
+    '/',
+    '/css/main.css',
+    '/css/util.css',
+    '/images/icons/favicon.ico',
+    '/images/bg-01.jpg',
+    '/js/app.js',
+    '/js/idb.js',
+    '/js/convert.js',
+    '/index.html'
+];
+
 self.addEventListener( "install" , function (event) { 
+    console.log('[ServiceWorker] Install');
     event.waitUntil(
         caches.open("cache1").then(function(cache) {
-            return cache.addAll(
-                [
-                    '/',
-                    '/css/main.css',
-                    '/css/util.css',
-                    '/images/icons/favicon.ico',
-                    '/images/bg-01.jpg',
-                    '/js/app.js',
-                    '/js/idb.js',
-                    '/js/convert.js',
-                    '/index.html'
-                ]
-            );
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
         })
     );
 });
